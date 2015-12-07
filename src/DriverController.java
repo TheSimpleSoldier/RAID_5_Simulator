642da8758f3dcf4d809b036343c0a2c99564229c
Joshua Leger <legermjoshua@gmail.com>
2015-12-07 10:15:21 -0700

    Fixed reconstruct

--- src/DriverController.java
+++ src/DriverController.java
@@ -166,7 +166,7 @@ public class DriverController
     public void replaceDrive(int drive)
     {
         System.out.println(drive);
-        drives[drive] = new Drive(driveSize, new File((drive) + ".txt"));
+        drives[drive] = new Drive(driveSize, new File(baseDir.getAbsolutePath() + "/" + drive + ".txt"));
         for (int i = 0; i < this.driveSize; i++)
         {
             byte newByte = 0;
